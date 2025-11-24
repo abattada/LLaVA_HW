0a9df9e0c13af6db81e63ce4bd5e8790c7ec5cb2
abat <abat52875@gmail.com>
2025-11-24 21:35:47 +0800

    New finetune

--- finetune.sh
+++ finetune.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 # 建議前面有 activate，不過你已經在對的 env 裡執行就沒差
 # conda activate /home/abat/conda_envs/swift
-
+#   --gradient_accumulation_steps 8 \
 python -m swift.cli.sft \
   --model_type llava1_5_hf \
   --model ./output/llava-1.5-7b-base \
   --train_type lora \
   --dataset data/jsonl/train.jsonl \
-  --num_train_epochs 30 \
-  --per_device_train_batch_size 1 \
-  --gradient_accumulation_steps 32 \
+  --num_train_epochs 10 \
+  --per_device_train_batch_size 8 \
   --learning_rate 1e-4 \
   --gradient_checkpointing true \
   --freeze_vit true \
+  --lora_rank 8 \
+  --lora_alpha 8\
   --torch_dtype bfloat16 \
-  --output_dir output/llava-vqarad-lora-swift \
-  --logging_steps 200 \
+  --output_dir output/llava-1.5-7b-finetune \
+  --logging_steps 50 \
   --logging_strategy epoch \
-  --save_strategy no
+  --save_strategy epoch \
+  --save_total_limit 1  \
+  --max_length 768
